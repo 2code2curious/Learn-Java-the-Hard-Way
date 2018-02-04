756875514e2758e9861024d29c99cbeb52a8e61d
Kamalpreet Grewal <grewalkamal005@gmail.com>
2018-02-04 14:17:38 -0500

    Add files for assignment 4 of the series

---
+++ A_Good_First_Program/src/Exercise2.java
@@ -0,0 +1,13 @@
+// Put two slashes ('//') at the beginning of one of the println() statements.
+
+public class Exercise2 {
+    public static void main(String[] args){
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello Again" );
+//        System.out.println( "I like typing this." );
+        System.out.println( "This is fun." );
+        System.out.println( "Yay! Printing." );
+        System.out.println( "I'd much rather you 'not'." );
+        System.out.println( "I \"said\" do not touch this." );
+    }
+}

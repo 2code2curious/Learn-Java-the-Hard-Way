6dc3f7435781202846fd54c402b169c8e13dc0fb
Kamalpreet Grewal <grewalkamal005@gmail.com>
2018-03-01 18:58:08 -0500

    Files for exercise 5 and 6

---
+++ A_Letter_To_Yourself/src/LetterToYourself.java
@@ -0,0 +1,14 @@
+public class LetterToYourself {
+    public static void main(String[] args){
+        System.out.println("+----------------------------------+");
+        System.out.println("|                              ### |");
+        System.out.println("|                              ### |");
+        System.out.println("|                              ### |");
+        System.out.println("|                                  |");
+        System.out.println("|                123 Street        |");
+        System.out.println("|                ABC City          |");
+        System.out.println("|                Canada, W12R12    |");
+        System.out.println("|                                  |");
+        System.out.println("+----------------------------------+");
+    }
+}
